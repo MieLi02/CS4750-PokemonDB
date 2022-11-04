7c103d5c12e4d74377f8c5a8988ce46c66bad79d
Da-Justin-Lin <justinlinda2001@gmail.com>
2022-11-03 22:13:27 -0400

    added login and signup function

--- classes/handler.php
+++ classes/handler.php
@@ -1,4 +1,6 @@
 <?php
+require "connect_database.php";
+require "pokemon_db.php";
 
 class Handler {
     private $command;
@@ -18,6 +20,9 @@ class Handler {
             case "login":
                 $this -> login();
                 break;
+            case "signup":
+                $this -> signup();
+                break;
             default:
                 $this -> index();
                 break;
@@ -31,7 +36,6 @@ class Handler {
 
     private function login() {
         include("views/login.php");
-        /*
         if (isset($_POST["email"])) {
             $data = $this->db->query("select * from recipick_user where email = ?;", "s", $_POST["email"]);
             $data_json = json_encode($data);
@@ -56,7 +60,37 @@ class Handler {
             }
         }
         include("login.php");
-        */
+    }
+
+    private function signup() {
+        if (isset($_POST["email"])) {
+            $data = $this->db->query("select * from recipick_user where email = ?;", "s", $_POST["email"]);
+            $data_json = json_encode($data);
+            if ($data === false) {
+                $error_msg = "Error checking for user";
+            }
+            else if ( ($_POST["email"] == "") || ($_POST["name"] == "") || ($_POST["password"] == "") ){
+                $error_msg = "Don't leave signup fields blank";
+            }
+            else if (!empty($data)) {
+                $error_msg = "Email is already in use";
+            } else {
+                $insert = $this->db->query("insert into recipick_user (name, email, password, num_recipes) values (?, ?, ?, ?);", 
+                        "ssss", $_POST["name"], $_POST["email"], 
+                        password_hash($_POST["password"], PASSWORD_DEFAULT), 0);
+                $id = $this->db->query("select id from recipick_user where email = ?;", "s", $_POST["email"]);
+                if ($insert === false) {
+                    $error_msg = "Error inserting user";
+                } else {
+                    session_start();
+                    $_SESSION["name"] =  $_POST["name"];
+                    $_SESSION["email"] =   $_POST["email"];
+                    $_SESSION["id"] = $id[0]["id"];
+                    header("Location: ?command=home");
+                }
+            }
+        }
+        include("login.php");
     }
     
 }
