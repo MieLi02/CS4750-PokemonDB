2ba0214abf96fa9c437ec40d7b48b77696c911a1
Da-Justin-Lin <justinlinda2001@gmail.com>
2022-11-03 20:51:53 -0400

    mmmmm

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokemon</title>
+    <link rel="stylesheet" type="text/css" href="styles/index.css" />
+</head>
+
+<body>
+    <div class = "upper">
+        <h1>
+            Pokemon
+        </h1>
+    </div>
+    <div class = "lower">
+        <p>
+            Get to know these pokemons!
+        </p>
+        <a class = "login">Login</a>
+    </div>
+</body>
+</html>
